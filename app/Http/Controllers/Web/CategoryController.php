4695146e2d0f3565e3738d6f0d0cf8a28808622a
Karan Talwar <karantalwar@uimatic.com>
2020-07-28 19:00:45 +0530

    Issue

--- app/Http/Controllers/Web/CategoryController.php
+++ app/Http/Controllers/Web/CategoryController.php
@@ -43,7 +43,7 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(CategoryRequest $request)
+    public function store(Request $request)
     {
         uploadImage($request->all()['image']);
         die();
