3af23616a5a74a0d1ea2a8fe0d06a967f92ae00e
Karan Talwar <karantalwar@uimatic.com>
2020-08-10 20:29:11 +0530

    added fields to image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,6 +8,10 @@ class Image extends Model
 {
     protected $fillable = ['url', 'imageable_id', 'imageable_type'];
 
+    protected $primaryKey = 'id';
+
+    public $keyType = 'string';
+
     public function imageable()
     {
         return $this->morphTo();
